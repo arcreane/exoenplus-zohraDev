6f54a1c38aa9dadd0379f5a9632dbcf855740e17
zohraDev <bach.zora@hotmail.Fr>
2020-11-02 14:45:38 +0100

    Premier commit

---
+++ src/com/game/projet/company/GuessNumber.java
@@ -0,0 +1,24 @@
+package com.game.projet.company;
+
+import java.util.Scanner;
+
+public class GuessNumber {
+
+        public static void main(String[] args) {
+
+            System.out.println("Selectionner un choix parmi ceux ci-dessous :..."+"\n"+"\t\t\t"+"Enter E : 10 essaies pour deviner le nombre."+"\n"+"\t\t\t"+"Enter J : Jouer jusqu'à troiver le nombre");
+
+            int nombreAleatoire = 1+ (int)(Math.random() * ((99) + 1));// generer un nombre aleatoir entre 1 et 100
+            //   System.out.println(nombreAleatoire);
+
+            int reponseUtilisateur;
+
+            System.out.println("Entrer un nombre entre 1 et 100: ");
+
+            Scanner Scan = new Scanner(System.in);
+
+
+
+
+
+        }}
